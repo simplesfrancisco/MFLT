47131aa3f25039f43df451556ef0adccb7d57b80
simplesfrancisco <umsimplesfrancisco@gmail.com>
2016-07-12 14:30:52 -0300

    MFLT
    Make Front Like This, first release, it is Pre Pre Alpha.

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var twig = require('gulp-twig');
+
+var myData = require('./app/data.json');
+gulp.task('twig', function(){
+    return gulp.src('app/views/*.twig')
+    .pipe(twig({
+        data: myData
+    }))
+    .pipe(gulp.dest('app'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('sass', function() {
+  return gulp.src('app/assets/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('watch', ['browserSync', 'sass', 'twig'], function (){
+    gulp.watch('app/views/**/*.twig', ['twig']);
+    gulp.watch('app/assets/scss/**/*.scss', ['sass']);
+    gulp.watch('app/views/**/*.twig', browserSync.reload); 
+    gulp.watch('app/assets/js/**/*.js', browserSync.reload); 
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+});
+
+
+gulp.task('MFLT', ['sass', 'watch',  'twig', 'browserSync'], function (){});
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Make Front Like This
+
+## What is need?
+- NodeJS
+
+## How to
+- Run "npm install --save-dev"
+- Run "gulp execute"
+
+### features
+- Twig listener
+- SASS/SCSS listener
+- Browser Sync (Reload)
+- Local data (app/data.json)
+
+### future
+- data.json will be sync with a mysql database
